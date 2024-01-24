9c2fa8323b8132f3ac084f012d6549994473c6f5
Ibrahium-Lashari <524Ilashari@frhsd.com>
2024-01-24 14:58:12 -0500

    TCA 2

---
+++ script.js
@@ -0,0 +1,36 @@
+// wait for the document to be ready before making call
+$(document).ready(function () {
+  // make a GET request to the JSON API using jQuery's .getJSON method
+  $.getJSON("schedule.json", function (data) {
+    // store the schedule data in a global variable
+    let scheduleData = data.schedule;
+
+    //Populate the table with all the schedule data
+    populateTable(scheduleData);
+
+    //Listen for changes to the dropDown Menu
+    $("#day-selector").on("change", function () {
+      let selectedDay = $(this).val();
+
+      //filter the schedule
+      let filteredData = scheduleData.filter(function (schedule) {
+        return selectedDay === "all" || schedule.days.includes(selectedDay);
+      });
+      populateTable(filteredData);
+    });
+  });
+  // Function to populate the table with schedule data
+  function populateTable(scheduleData) {
+    $("#schedule-table-body").empty();
+
+    $.each(scheduleData, function (i, schedule) {
+      let row = "<tr>";
+      row += "<td>" + schedule.class_name + "</td>";
+      row += "<td>" + schedule.teacher_name + "</td>";
+      row += "<td>" + schedule.room_number + "</td>";
+      row += "<td>" + schedule.days.join(", ") + "</td>";
+      row += "</tr>";
+      $("#schedule-table-body").append(row);
+    });
+  }
+});
