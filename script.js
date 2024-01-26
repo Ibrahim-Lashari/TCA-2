e9bda594ac872e03ed94087bef7b113d44aec2ab
Ibrahium-Lashari <524Ilashari@frhsd.com>
2024-01-26 13:47:58 -0500

    almost finished, functions don't work at the same time

--- script.js
+++ script.js
@@ -18,6 +18,27 @@ $(document).ready(function () {
       });
       populateTable(filteredData);
     });
+
+    $("#student-selector").on("change", function () {
+      let selectedStudent = $(this).val();
+
+      //filter the schedule
+      let filteredData = scheduleData.filter(function (schedule) {
+        return selectedStudent === "all" || schedule.student_name.includes(selectedStudent);
+      });
+      populateTable(filteredData);
+    });
+
+    $("#wing-selector").on("change", function () {
+      let selectedWing = $(this).val();
+
+      //filter the schedule
+      let filteredData = scheduleData.filter(function (schedule) {
+        return selectedWing === "all" || schedule.wing.includes(selectedWing);
+      });
+      populateTable(filteredData);
+    });
+
   });
   // Function to populate the table with schedule data
   function populateTable(scheduleData) {
@@ -25,9 +46,11 @@ $(document).ready(function () {
 
     $.each(scheduleData, function (i, schedule) {
       let row = "<tr>";
+      row += "<td>" + schedule.student_name + "</td>";
       row += "<td>" + schedule.class_name + "</td>";
       row += "<td>" + schedule.teacher_name + "</td>";
       row += "<td>" + schedule.room_number + "</td>";
+      row += "<td>" + schedule.wing + "</td>";
       row += "<td>" + schedule.days.join(", ") + "</td>";
       row += "</tr>";
       $("#schedule-table-body").append(row);
